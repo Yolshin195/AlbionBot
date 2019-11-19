1d2add519685cca33253ae8da8f7f57c449babb2
Alexey Yolshin <elshin195@gmail.com>
2019-11-19 19:28:09 +0500

    add lib

---
+++ src/lib/Client.py
@@ -0,0 +1,33 @@
+import socket
+
+class Client:
+    def __init__(self, host_name, port, protacol, logger=print):
+        self.host_name = host_name
+        self.port = port
+        self.protacol = protacol
+        self.logger = logger
+
+    def create_socket(self):
+        client_sock = socket.socket(socket.AF_INET,
+                                    socket.SOCK_STREAM)
+        return client_sock
+
+    def request(self, body):
+        client_sock = self.create_socket()
+        client_sock.connect((self.host_name, self.port))
+
+        self.protacol.write(client_sock, body)
+        respons = self.protacol.read(client_sock)
+
+        client_sock.close()
+        return respons
+
+if __name__ == '__main__':
+    from Protacol import Protacol
+
+    protacol = Protacol(10)
+    client = Client('localhost', 4001, protacol)
+    data = client.request('<getimg/>')
+
+    print('Received', repr(data))
+
---
+++ src/lib/Server.py
@@ -0,0 +1,66 @@
+import socket
+import sys
+import time
+
+class Server:
+    def __init__(self, port, protacol, logger=print):
+        self.logger = logger
+        self.port = port
+        self.protacol = protacol 
+
+    #def __del__(self):
+    #    self.serv_sock.close()
+
+
+    def run_server(self):
+        self.serv_sock = self.create_server_sock(self.port)
+        cid = 0
+        while True:
+            client_sock = self.accept_client_conn(self.serv_sock, cid)
+            self.server_client(client_sock, cid)
+            cid += 1
+
+    def create_server_sock(self, server_port):
+        serv_sock = socket.socket(socket.AF_INET,
+                                  socket.SOCK_STREAM,
+                                  proto=0)
+        serv_sock.bind(('', server_port))
+        serv_sock.listen()
+
+        self.logger(f'Server running on port {server_port}')
+
+        return serv_sock
+
+    def accept_client_conn(self, server_sock, cid):
+        client_sock, client_addr = server_sock.accept()
+        self.logger(f'Client #{cid} connect '
+                    f'{client_addr[0]}:{client_addr[1]}')
+        return client_sock
+
+    def server_client(self, client_sock, cid):
+        request = self.read_request(client_sock)
+        if request is None:
+            self.logger(f'Client #{cid} unexpectedly disconnected')
+        else:
+            response = self.handle_request(request)
+            self.write_response(client_sock, response, cid)
+
+    def read_request(self, client_sock):
+        return self.protacol.read(client_sock)
+
+    def handle_request(self, request):
+        time.sleep(5)
+        return request[::-1]
+
+    def write_response(self, client_sock, response, cid):
+        self.protacol.write(client_sock, response)
+        client_sock.close()
+        self.logger(f'Client #{cid} has been served')
+
+
+if __name__== '__main__':
+    from Protacol import Protacol
+
+    protacol = Protacol(10)
+    server = Server(4000, protacol)
+    server.run_server()
